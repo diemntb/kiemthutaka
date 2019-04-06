c839e9080e690fd308e81bbcec8cec1b7a2bdf93
Bích Diễm <ntbdiem01@gmail.com>
2019-04-06 23:26:11 +0700

    commit testDuplicate user

--- test-case/TestUser.php
+++ test-case/TestUser.php
@@ -15,4 +15,16 @@ class TestUser extends PHPUnit_Framework_TestCase
         $this->assertTrue($user->login() == true, 'Test Login');
 
     }
+
+    public function testDuplicate(){
+        $username = "diem"; // diem là có rồi -> true : trùng
+        $pass = "123456";
+        $name = "Diễm";
+        $email = "ntbdiem01@gmail.com";
+        $dob = "04-04-1994";
+        //$id, $username, $password, $name, $email, $dob, $permission
+        $user = new User(0, $username, $pass, $name, $email, $dob, 0);
+
+        $this->assertTrue($user->checkDuplicate($username) == true); // true => trùng, false => chưa trùng
+    }
 }
