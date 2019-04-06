e4f5434365207d26b724a02fb6d7683fee26e376
Bích Diễm <ntbdiem01@gmail.com>
2019-04-06 23:08:30 +0700

    commit testCountProductsByBrand

--- test-case/TestProducts.php
+++ test-case/TestProducts.php
@@ -10,4 +10,23 @@ class TestProducts extends PHPUnit_Framework_TestCase
 
         $this->assertTrue(sizeof($product) == 13);
     }
+
+    public function testCountProductsByBrand(){
+        /*
+            1	Sen đá
+            2	Xương rồng
+            3	Terrarium
+            4	Chậu
+            5	Phụ kiện trang trí
+         * */
+        $search = 1;
+        $product = Products::loadProductsByCatId($search);
+
+        $this->assertTrue(sizeof($product) == 5); /* Count Cat = 1*/
+//        $this->assertTrue(sizeof($product) == 4); /* Count Cat = 2*/
+//        $this->assertTrue(sizeof($product) == 4); /* Count Cat = 3*/
+//        $this->assertTrue(sizeof($product) == 0); /* Count Cat = 4*/
+//        $this->assertTrue(sizeof($product) == 0); /* Count Cat = 5*/
+    }
+
 }
